50f90acbad4c7fae12957a64e7083ad8dbc911e0
FarlenUrena <69374529+FarlenUrena@users.noreply.github.com>
2023-05-20 19:45:16 -0600

    Create anime repository

---
+++ package/anime_repository/lib/anime_repository.dart
@@ -0,0 +1,4 @@
+/// My new anime repository package
+library anime_repository;
+
+export 'src/anime_repository.dart';
---
+++ package/anime_repository/lib/src/anime_repository.dart
@@ -0,0 +1,7 @@
+/// {@template anime_repository}
+/// My new anime repository package
+/// {@endtemplate}
+class AnimeRepository {
+  /// {@macro anime_repository}
+  const AnimeRepository();
+}
