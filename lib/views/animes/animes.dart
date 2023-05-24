bfbb110cf412f1f5fb5c9655a49b2ce636e7dcc8
FarlenUrena <69374529+FarlenUrena@users.noreply.github.com>
2023-05-24 16:26:12 -0600

    Updates

--- lib/views/animes/animes.dart
+++ lib/views/animes/animes.dart
@@ -35,62 +35,82 @@ class Animes extends StatelessWidget {
         return Scaffold(
           body: SingleChildScrollView(
             child: Center(
-              child: GridView.builder(
-                padding: const EdgeInsets.all(16),
-                shrinkWrap: true,
-                physics: const NeverScrollableScrollPhysics(),
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 4,
-                  crossAxisSpacing: 40,
-                  mainAxisSpacing: 40,
-                ),
-                itemCount: animesFiltered.length,
-                itemBuilder: (context, index) {
-                  final anime = animesFiltered[index];
-
-                  return Card(
-                    key: ValueKey(anime),
-                    color: Color.fromRGBO(163, 65, 61, 1),
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          anime.title.english,
-                          style: const TextStyle(
-                            color: Colors.white,
-                            fontSize: 20,
-                          ),
-                          textAlign: TextAlign.center,
-                        ),
-                        Text(
-                          'ID: ${anime.id}',
-                          style: const TextStyle(
-                            color: Colors.white,
-                            fontSize: 14,
-                          ),
-                          textAlign: TextAlign.center,
-                        ),
-                        Text(
-                          'Type: ${anime.type}',
-                          style: const TextStyle(
-                            color: Colors.white,
-                            fontSize: 12,
-                          ),
-                          textAlign: TextAlign.center,
-                        ),
-                        Text(
-                          'Genres: ${anime.genres.join(", ")}',
-                          style: const TextStyle(
-                            color: Colors.white,
-                            fontSize: 10,
-                          ),
-                          textAlign: TextAlign.center,
-                        ),
-                      ],
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Text(
+                      '$genreSelected Genre Animes',
+                      style: TextStyle(
+                        fontSize: 35,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                  );
-                },
+                  ),
+                  GridView.builder(
+                    padding: const EdgeInsets.all(16),
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 4,
+                      crossAxisSpacing: 40,
+                      mainAxisSpacing: 40,
+                    ),
+                    itemCount: animesFiltered.length,
+                    itemBuilder: (context, index) {
+                      final anime = animesFiltered[index];
+
+                      return Card(
+                        key: ValueKey(anime),
+                        color: Color.fromRGBO(163, 65, 61, 1),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              anime.title.english,
+                              style: const TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                            Text(
+                              'ID: ${anime.id}',
+                              style: const TextStyle(
+                                color: Colors.white,
+                                fontSize: 14,
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                            Text(
+                              'Type: ${anime.type}',
+                              style: const TextStyle(
+                                color: Colors.white,
+                                fontSize: 12,
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                            Text(
+                              'Genres: ${anime.genres.join(", ")}',
+                              style: const TextStyle(
+                                color: Colors.white,
+                                fontSize: 10,
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                  ),
+                  const Text('End of Animes',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 30,
+                      )),
+                ],
               ),
             ),
           ),
